b0925a48df17c85ff67cd4a12205f47582b5f76c
JobinSamuel <86288918+JobinSamuel@users.noreply.github.com>
2022-08-28 15:52:20 +0530

    Update Naivebayes.R

--- Naivebayes.R
+++ Naivebayes.R
@@ -1,4 +1,5 @@
-####Question -1
+####Prepare a classification model using the Naive Bayes algorithm for the salary dataset. Train and test datasets are given separately. Use both for model building. 
+
 library(e1071)
 library(gmodels)
 library(scales)
@@ -39,7 +40,8 @@ test_acc
 acc <- percent((11211+3613)/15060)
 acc
 
-####Question -2
+####This dataset contains information of users in a social network. This social network has several business clients which can post ads on it. One of the clients has a car company which has just launched a luxury SUV for a ridiculous price. Build a Bernoulli Naïve Bayes model using this dataset and classify which of the users of the social network are going to purchase this luxury SUV. 1 implies that there was a purchase and 0 implies there wasn’t a purchase.
+
 #Read data
 sn <-read.csv("/Users/jobinsamuel/Desktop/Assignments/Naive bias/Datasets_Naive Bayes/NB_Car_Ad.csv")
 #Spliting data into two train and test
@@ -62,7 +64,7 @@ accu
 test_accu <- mean(predic == test_sn$Purchased)
 test_accu #Here we are already getting an accuracy of 93.3%
 
-####Question -3
+####In this case study, you have been given Twitter data collected from an anonymous twitter handle. With the help of a Naïve Bayes model, predict if a given tweet about a real disaster is real or fake.
 Tw <- read.csv("/Users/jobinsamuel/Desktop/Assignments/Naive bias/Datasets_Naive Bayes/Disaster_tweets_NB.csv")
 str(Tw)
 Tw_f <- factor(Tw$target)
